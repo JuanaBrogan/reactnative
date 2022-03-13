31c484c033d9e65d834239dc59f45e8d8607d23e
Genie Brogan <Genie.Brogan@outlook.com>
2022-03-13 16:22:46 -0400

    So far

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 export default function App() {
   return (
-      <Main />
+      <Provider store={store}>
+        <Main/>
+      </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/ContactComponent.js
@@ -0,0 +1,31 @@
+import { Card } from "react-native-elements";
+import React, { Component } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { render } from "react-dom";
+
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  static navigationOptions = {
+    title: "Contact Us",
+  };
+
+  render() {
+    return (
+      <ScrollView>
+        <Card wrapperStyle={{ margin: 20 }} title={"Contact Information"}>
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text>U.S.A</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+}
+
+export default Contact;
